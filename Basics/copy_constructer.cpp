e3a723b684dbd8cf505b0a1ecb06bf0457701dd1
Coder-sam-007 <padamzain178@gmail.com>
2022-07-23 19:12:46 +0530

    copy constructor success

--- Basics/copy_constructer.cpp
+++ Basics/copy_constructer.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 class data
 {
-    int data1;
+    int datas;
 
 public:
     data(int d)
     {
-        data1 = d;
-        cout << "The data is:" << data1 << endl;
+        datas = d;
+        cout << "The data is:" << datas << endl;
     }
 
     data(data &data1)
     {
+        datas = data1.datas;
+        cout << "The data is:" << datas << endl;
     }
 };
 
@@ -22,6 +24,10 @@ int main()
     data data1(10);
 
     data data2(data1);
+
+    data data3(data2);
+
+    data data4(data3);
 
     return 0;
 }
