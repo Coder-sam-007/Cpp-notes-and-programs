a1df554148ed9840f3e8fcfe12b89988967161c4
Coder-sam-007 <padamzain178@gmail.com>
2022-08-03 13:16:59 +0530

    static updated

---
+++ Basics/static.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class apple
+{
+public:
+  static int count;
+
+  apple()
+  {
+    count++;
+  };
+};
+
+int apple::count = 0;
+
+int main()
+{
+  apple obj1;
+  cout << "Total no. of apple collected till now is: " << obj1.count << endl;
+  apple obj2;
+  cout << "Total no. of apple collected till now is: " << obj2.count << endl;
+  apple obj3;
+  cout << "Total no. of apple collected till now is: " << obj3.count << endl;
+  apple obj4;
+  cout << "Total no. of apple collected till now is: " << obj4.count << endl;
+}
