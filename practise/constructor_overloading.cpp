ee03b422c2827c41b51953a6c577a840c68cbff3
Coder-sam-007 <padamzain178@gmail.com>
2022-08-04 18:12:08 +0530

    practise_stuff_updated

---
+++ practise/constructor_overloading.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class sum
+{
+private:
+  int num1;
+  int num2;
+  float num3;
+  float num4;
+
+public:
+  sum(int n1, int n2)
+  {
+    num1 = n1;
+    num2 = n2;
+
+    cout << "the sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << endl;
+  }
+  sum(double n1, double n2)
+  {
+    num3 = n1;
+    num4 = n2;
+
+    cout << "the sum of " << num3 << " and " << num4 << " is: " << num3 + num4 << endl;
+  }
+};
+
+int main()
+{
+  sum pair1(12, 21);
+  sum pair2(10.5, 10.5);
+
+  return 0;
+}
